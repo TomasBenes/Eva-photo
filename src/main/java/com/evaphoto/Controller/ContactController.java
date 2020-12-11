95ad76f9b61f97beb2233a5d28642edae461bf9d
TomasBenes <tomasbenes123gmail.com>
2020-12-11 16:01:33 +0100

    implemented working contact form with possibility for an attachment

--- src/main/java/com/evaphoto/Controller/ContactController.java
+++ src/main/java/com/evaphoto/Controller/ContactController.java
@@ -1,14 +1,22 @@
 package com.evaphoto.Controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.InputStreamSource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 @Controller
 public class ContactController {
@@ -22,23 +30,33 @@ public class ContactController {
     }
 
     @PostMapping("/kontakt")
-    public String submitEmail (@RequestParam String name, @RequestParam String email, @RequestParam String subject, @RequestParam String content) {
-        /*String name = request.getParameter("name");HttpServletRequest request
-        String email = request.getParameter("email");
-        String subject = request.getParameter("subject");
-        String content = request.getParameter("content");*/
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("tominobenes@gmail.com");
-        message.setTo("tomasbenes123@gmail.com");
+    public String submitEmail (@RequestParam String name, @RequestParam String email, @RequestParam String subject, @RequestParam String content, @RequestParam MultipartFile attachment) throws MessagingException, UnsupportedEncodingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
         String mailSubject = name + "odeslal email";
-        String mailContent = "Jmeno odesilatele: " + name + "\n";
-        mailContent += "Email odesilatele: " + email + "\n";
-        mailContent += "Predmet: " + subject + "\n";
-        mailContent += "Obsah: " + content + "\n";
+        String mailContent = "<p><b>Jmeno odesilatele:</b> " + name + "</p>";
+        mailContent += "<p><b>Email odesilatele:</b> " + email + "</p>";
+        mailContent += "<p><b>Predmet:</b> " + subject + "</p>";
+        mailContent += "<p><b>Obsah:</b> " + content + "</p>";
 
-        message.setSubject(mailSubject);
-        message.setText(mailContent);
+        helper.setFrom("tominobenes@gmail.com", "Eva photo");
+        helper.setTo("tomasbenes123@gmail.com");
+        helper.setSubject(mailSubject);
+        helper.setText(mailContent, true);
+
+        if (!attachment.isEmpty()) {
+            String fileName = StringUtils.cleanPath(attachment.getOriginalFilename());
+
+            InputStreamSource source = new InputStreamSource() {
+                @Override
+                public InputStream getInputStream() throws IOException {
+                    return attachment.getInputStream();
+                }
+            };
+            helper.addAttachment(fileName, source);
+        }
 
         this.mailSender.send(message);
 
